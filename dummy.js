13c9d531796b5c1581ccea5652518de9d0afc483
Ashokkumar2232 <srvashok22@gmail.com>
2023-05-09 14:14:42 +0530

    new changes to new people b Section

--- dummy.js
+++ dummy.js
@@ -25,4 +25,32 @@ const speak = () => {
   console.log(__filename);
   
   // no access to DOM methods
+  //console.log(document.queryS
+  const speak = () => {
+    console.log('hello, ninjas');
+  }
+  
+  speak();
+  
+  // Global Object
+  
+  // console.log(global);
+  
+  // global.setTimeout(() => {
+  //   console.log('in the timeout');
+  // }, 3000);
+ /*   */
+  setTimeout(() => {
+    console.log('in the timeout');
+    clearInterval(int);
+  }, 15000);
+
+  const int = setInterval(() => {
+    console.log('vels, in the interval');
+  }, 2000);
+  
+  console.log(__dirname);
+  console.log(__filename);
+  
+  // no access to DOM methods
   //console.log(document.querySelector);
